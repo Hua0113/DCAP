0d1e042766927d323db1a62b2a3215905abd0a79
Hua0113 <54987315+Hua0113@users.noreply.github.com>
2019-10-12 10:03:29 +0800

    Update autoencoder_DCAP.py

--- autoencoder_DCAP.py
+++ autoencoder_DCAP.py
@@ -23,11 +23,10 @@ scale = 0.0001
 # tf Graph input (only pictures)
 X = tf.placeholder("float", [None, n_input])
 
-# 用字典的方式存储各隐藏层的参数
-n_hidden_1 = 500 # 第一编码层神经元个数
-n_hidden_2 = 200 # 第二编码层神经元个数
-# 权重和偏置的变化在编码层和解码层顺序是相逆的
-# 权重参数矩阵维度是每层的 输入*输出，偏置参数维度取决于输出层的单元数
+
+n_hidden_1 = 500 # 
+n_hidden_2 = 200 # 
+
 weights = {
     'encoder_h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
     'encoder_h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
@@ -41,8 +40,7 @@ biases = {
     'decoder_b2': tf.Variable(tf.random_normal([n_input])),
 }
 
-# 每一层结构都是 xW + b
-# 构建编码器
+
 def encoder(x):
     layer_1 = tf.nn.tanh(tf.add(tf.matmul(x, weights['encoder_h1']),
                                    biases['encoder_b1']))
@@ -51,7 +49,7 @@ def encoder(x):
     return layer_2
 
 
-# 构建解码器
+
 def decoder(x):
     layer_1 = tf.nn.tanh(tf.add(tf.matmul(x, weights['decoder_h1']),
                                    biases['decoder_b1']))
@@ -59,7 +57,7 @@ def decoder(x):
                                    biases['decoder_b2']))
     return layer_2
 
-# 构建模型
+
 #encoder_op = encoder(X)
 #decoder_op = decoder(encoder_op)
 
@@ -81,12 +79,12 @@ fc_3_dropout = tf.layers.dropout(inputs=fc_3_out, rate=dropout)
 decoder_op = tf.layers.dense(inputs=fc_3_dropout, units=n_input)
 ##################################################################
 
-# 预测
+
 y_pred = decoder_op
 y_true = X
 
-# 定义代价函数和优化器
-cost = tf.reduce_mean(tf.pow(y_true - y_pred, 2))#+lossL #最小二乘法
+
+cost = tf.reduce_mean(tf.pow(y_true - y_pred, 2))#+lossL 
 l2_loss = tf.losses.get_regularization_loss()
 optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost+l2_loss)
 
@@ -98,8 +96,8 @@ with tf.Session() as sess:
     else:
         init = tf.global_variables_initializer()
     sess.run(init)
-    # 首先计算总批数，保证每次循环训练集中的每个样本都参与训练，不同于批量训练
-    total_batch = int(len(tcga_input)/batch_size) #总批数
+  
+    total_batch = int(len(tcga_input)/batch_size) 
     for epoch in range(training_epochs):
         for i in range(total_batch):
             # tch_xs, batch_ys = mnist.train.next_batch(batch_size)  # max(x) = 1, min(x) = 0
